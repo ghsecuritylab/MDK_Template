e34eaaa81435b3bd895f159764a6f4208d019e24
ZBT1314douhao <zbt1314douhao@live.com>
2016-08-30 08:22:55 +0800

    统一设定默认下载方式为SW；优化等级level 0， Signed-off-by: ZBT1314douhao <zbt1314douhao@live.com>

--- TemplateV1.0_CMSIS_bare/Project/Objects/system_stm32f10x.d
+++ TemplateV1.0_CMSIS_bare/Project/Objects/system_stm32f10x.d
@@ -1,14 +1,14 @@
 .\objects\system_stm32f10x.o: RTE\Device\STM32F103RB\system_stm32f10x.c
 .\objects\system_stm32f10x.o: ..\Applications\stm32f10x.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm3.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cm3.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cminstr.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\cmsis_armcc.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cmfunc.h
 .\objects\system_stm32f10x.o: ..\Applications\system_stm32f10x.h
 .\objects\system_stm32f10x.o: ..\Applications\stm32f10x_conf.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\StdPeriph_Driver\inc\stm32f10x_exti.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\Keil\STM32F1xx_DFP\2.1.0\Device\StdPeriph_Driver\inc\stm32f10x_exti.h
 .\objects\system_stm32f10x.o: ..\Applications\stm32f10x.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
-.\objects\system_stm32f10x.o: C:\mySoftware\MDK\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\StdPeriph_Driver\inc\misc.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\Keil\STM32F1xx_DFP\2.1.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\Keil\STM32F1xx_DFP\2.1.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\system_stm32f10x.o: C:\My Softwares\MDK_520\ARM\PACK\Keil\STM32F1xx_DFP\2.1.0\Device\StdPeriph_Driver\inc\misc.h
