1c2ad40cad31ac419285cc5827106ff9621fad60
ZBT1314douhao <zbt1314douhao@live.com>
2018-03-24 14:24:55 +0800

    添加.o文件，以免下载后要重新编译

---
+++ TemplateV2.1_LibV3.5_RT-ThreadV2.1.0/rt-thread-2.1.0/bsp/stm32f107/build/dataqueue.d
@@ -0,0 +1,14 @@
+.\build\dataqueue.o: ..\..\components\drivers\src\dataqueue.c
+.\build\dataqueue.o: ..\..\include\rtthread.h
+.\build\dataqueue.o: .\rtconfig.h
+.\build\dataqueue.o: ..\..\include\rtdebug.h
+.\build\dataqueue.o: ..\..\include\rtdef.h
+.\build\dataqueue.o: C:\mySoftware\MDK\ARM\ARMCC\Bin\..\include\stdarg.h
+.\build\dataqueue.o: ..\..\include\rtservice.h
+.\build\dataqueue.o: ..\..\include\rtm.h
+.\build\dataqueue.o: ..\..\include\rtthread.h
+.\build\dataqueue.o: ..\..\components\drivers\include\rtdevice.h
+.\build\dataqueue.o: ..\..\components\drivers\include\drivers/spi.h
+.\build\dataqueue.o: C:\mySoftware\MDK\ARM\ARMCC\Bin\..\include\stdlib.h
+.\build\dataqueue.o: ..\..\components\drivers\include\drivers/serial.h
+.\build\dataqueue.o: ..\..\include\rthw.h
